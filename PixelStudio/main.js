4bbaee1787f8a15db348cb15fead7ad8280fed6a
Ebe133 <6050520@mborijnland.nl>
2026-06-15 16:19:17 +0200

    3: Application entry point - Add main.js

---
+++ PixelStudio/main.js
@@ -0,0 +1,8 @@
+/**
+ * main.js
+ * Startpunt van de applicatie.
+ * Maakt één App-object aan zodra de DOM klaar is.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new App();
+});
